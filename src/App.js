82960bfe124f5d8b260f526818c0f71ab655d732
T-queen <142670502+T-queen@users.noreply.github.com>
2023-12-24 11:29:19 +0100

    Update App.js by adding GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/taiwo-oresanya-b00209135/" target="_blank" rel="noreferrer">Taiwo Oresanya</a>  
+      {""} and is {""}
+      <a href="https://github.com/T-queen/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </footer>
+     
     </div>
   );
 }
 
-export default App;
+
